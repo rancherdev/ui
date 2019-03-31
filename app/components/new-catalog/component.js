6a5ce7db95c95a54e8fbe3618a2eb24d59f60a28
loganhz <logan@rancher.com>
2019-03-31 19:51:46 +0800

    Enable registry
    https://github.com/rancher/rancher/issues/19086

--- app/components/new-catalog/component.js
+++ app/components/new-catalog/component.js
@@ -391,6 +391,6 @@ export default Component.extend(NewOrEdit, CatalogApp, {
   shouldFallBackToYaml() {
     const questions = get(this, 'selectedTemplateModel.allQuestions') || [];
 
-    return !!questions.some((question) => get(question, 'type') === 'password' && !!isNumeric(get(question, 'answer')));
+    return !!questions.some((question) => get(question, 'type') === 'password' && !!isNumeric(get(question, 'answer')) && get(question, 'answer') !== '');
   },
 });
